26bb7d30238cb3a5b4b02c1d9deb0f9957968e8c
Mariana Facchina <marianafacchina@marianas-macbook-pro.local>
2018-05-19 14:11:18 -0300

    Initial Commit

---
+++ ImageRecognition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageRecognition
+//
+//  Created by Mariana Facchina on 19/05/2018.
+//  Copyright © 2018 facchina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
